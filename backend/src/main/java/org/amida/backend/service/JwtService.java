7decc4d00b449e09379f3d8f4966803ee70b6d97
amida <cain.wells@mail.ru>
2024-11-03 17:38:28 +0300

    feat: configure JwtService service class

--- backend/src/main/java/org/amida/backend/service/JwtService.java
+++ backend/src/main/java/org/amida/backend/service/JwtService.java
@@ -1,4 +1,81 @@
 package org.amida.backend.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+
+    @Value("${jwt.lifetime}")
+    private Duration jwtExpiration;
+
+    public String extractUsername(String token) {
+        return extractClaim(token.trim(), Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String, String> extraClaims, UserDetails userDetails) {
+        Date issuedDate = new Date();
+        Date expiredDate = new Date(issuedDate.getTime() + jwtExpiration.toMillis());
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(issuedDate)
+                .expiration(expiredDate)
+                .signWith(getKey())
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(Collections.emptyMap(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+
 }
