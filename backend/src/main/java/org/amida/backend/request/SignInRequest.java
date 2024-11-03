ca4ca07b42e781647c25286572a8c98c91e3d38e
amida <cain.wells@mail.ru>
2024-11-03 17:32:41 +0300

    add SignInRequest request dto class

--- backend/src/main/java/org/amida/backend/request/SignInRequest.java
+++ backend/src/main/java/org/amida/backend/request/SignInRequest.java
@@ -1,4 +1,14 @@
 package org.amida.backend.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class SignInRequest {
+
+    @NotBlank(message = "")
+    private String username;
+
+    @NotBlank(message = "")
+    private String password;
 }
