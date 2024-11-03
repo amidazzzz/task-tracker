8099174701be21bec5d41cbbb4688b0cefc79e88
amida <cain.wells@mail.ru>
2024-11-03 17:38:59 +0300

    feat: add UsernameAlreadyExistsException exception class

--- backend/src/main/java/org/amida/backend/exception/UsernameAlreadyExistsException.java
+++ backend/src/main/java/org/amida/backend/exception/UsernameAlreadyExistsException.java
@@ -1,4 +1,7 @@
 package org.amida.backend.exception;
 
-public class UsernameAlreadyExistsException {
+public class UsernameAlreadyExistsException extends RuntimeException{
+    public UsernameAlreadyExistsException(String message){
+        super(message);
+    }
 }
