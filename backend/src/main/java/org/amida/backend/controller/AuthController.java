97d476be3862aa4dd570d3f17b835c63d4a88583
amida <cain.wells@mail.ru>
2024-11-04 20:10:02 +0300

    feat: configure AuthController controller class

--- backend/src/main/java/org/amida/backend/controller/AuthController.java
+++ backend/src/main/java/org/amida/backend/controller/AuthController.java
@@ -1,4 +1,32 @@
 package org.amida.backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.amida.backend.request.SignInRequest;
+import org.amida.backend.request.SignUpRequest;
+import org.amida.backend.response.ApiResponse;
+import org.amida.backend.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+@Slf4j
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register") // fix http method
+    public ResponseEntity<ApiResponse> register(@RequestBody SignUpRequest request){
+        return ResponseEntity.ok(authService.signUp(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@RequestBody SignInRequest request){
+        return ResponseEntity.ok(authService.singIn(request));
+    }
 }
