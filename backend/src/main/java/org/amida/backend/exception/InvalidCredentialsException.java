8be3985a03d1246ea844f97f34a9d5526f8fd268
amida <cain.wells@mail.ru>
2024-11-04 13:34:05 +0300

    feat: add InvalidCredentialsException exception class

--- backend/src/main/java/org/amida/backend/exception/InvalidCredentialsException.java
+++ backend/src/main/java/org/amida/backend/exception/InvalidCredentialsException.java
@@ -1,4 +1,7 @@
 package org.amida.backend.exception;
 
-public class InvalidCredentialsException {
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
 }
