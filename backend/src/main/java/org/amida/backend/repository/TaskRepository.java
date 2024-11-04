1b2caa82af3d76cabd22bc47005820af66ac5f38
amida <cain.wells@mail.ru>
2024-11-04 20:33:57 +0300

    feat: configure TaskRepository repository class

--- backend/src/main/java/org/amida/backend/repository/TaskRepository.java
+++ backend/src/main/java/org/amida/backend/repository/TaskRepository.java
@@ -1,4 +1,16 @@
 package org.amida.backend.repository;
 
-public interface TaskRepository {
+import org.amida.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByOwnerId(Long userId);
+
+    Task findByOwnerIdAndId(Long userId, Long taskId);
+
+    Optional<Task> findByTitle(String title);
 }
