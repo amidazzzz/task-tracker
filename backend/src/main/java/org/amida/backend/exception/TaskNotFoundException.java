43499b4cdfa266b3652d6fbb184141d22b4403be
amida <cain.wells@mail.ru>
2024-11-04 20:34:13 +0300

    feat: add TaskNotFoundException exception class

--- backend/src/main/java/org/amida/backend/exception/TaskNotFoundException.java
+++ backend/src/main/java/org/amida/backend/exception/TaskNotFoundException.java
@@ -1,4 +1,7 @@
 package org.amida.backend.exception;
 
-public class TaskNotFoundException {
+public class TaskNotFoundException extends RuntimeException{
+    public TaskNotFoundException(String message){
+        super(message);
+    }
 }
