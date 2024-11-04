21a87ae8a7129f06d8ea7254c2e0c6dab8830b47
amida <cain.wells@mail.ru>
2024-11-04 13:13:50 +0300

    feat: add sign in method in AuthService.java

--- backend/src/main/java/org/amida/backend/service/AuthService.java
+++ backend/src/main/java/org/amida/backend/service/AuthService.java
@@ -2,6 +2,7 @@ package org.amida.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.amida.backend.model.User;
+import org.amida.backend.request.SignInRequest;
 import org.amida.backend.request.SignUpRequest;
 import org.amida.backend.response.ApiResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,10 +21,18 @@ public class AuthService {
         User user = User.builder()
                 .username(request.getUsername())
                 .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
+                .password(request.getPassword())
                 .build();
 
         userService.registerUser(user);
+    }
+
+    public ApiResponse singIn(SignInRequest request){
+        return ApiResponse.builder()
+                .token(userService.authUser(request.getUsername(), request.getPassword()))
+                .success(true)
+                .message(String.format("The user %s logged in successfully", request.getUsername()))
+                .build();
     }
 
 }
