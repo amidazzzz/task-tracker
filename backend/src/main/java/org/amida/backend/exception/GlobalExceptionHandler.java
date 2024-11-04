138776d3d6b7f797dcab0ec87de7e553352b32d7
amida <cain.wells@mail.ru>
2024-11-04 20:07:22 +0300

    feat: configure GlobalExceptionHandler class

--- backend/src/main/java/org/amida/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/amida/backend/exception/GlobalExceptionHandler.java
@@ -1,4 +1,33 @@
 package org.amida.backend.exception;
 
+import org.amida.backend.response.ApiResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ApiResponse handleUsernameAlreadyExists(UsernameAlreadyExistsException ex) {
+        return ApiResponse.builder()
+                .message(ex.getMessage())
+                .success(false)
+                .build();
+    }
+
+    @ExceptionHandler(EmailAlreadyTakenException.class)
+    public ApiResponse handleEmailAlreadyTaken(EmailAlreadyTakenException ex) {
+        return ApiResponse.builder()
+                .message(ex.getMessage())
+                .success(false)
+                .build();
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ApiResponse handleInvalidCredentials(InvalidCredentialsException ex) {
+        return ApiResponse.builder()
+                .message(ex.getMessage())
+                .success(false)
+                .build();
+    }
 }
