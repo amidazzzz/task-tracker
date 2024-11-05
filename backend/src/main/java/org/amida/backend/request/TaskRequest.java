994b9901ae9f06a67429965c3175f79a323654ed
amida <cain.wells@mail.ru>
2024-11-05 21:23:20 +0300

    feat: add TaskRequest dto class

--- backend/src/main/java/org/amida/backend/request/TaskRequest.java
+++ backend/src/main/java/org/amida/backend/request/TaskRequest.java
@@ -1,4 +1,11 @@
 package org.amida.backend.request;
 
+import lombok.Data;
+import org.amida.backend.model.Status;
+
+@Data
 public class TaskRequest {
+    private String title;
+    private String description;
+    private Status status;
 }
