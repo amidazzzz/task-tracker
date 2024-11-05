8220bd7a628fe97daf5698625ca874608f1ceaa6
amida <cain.wells@mail.ru>
2024-11-05 21:34:52 +0300

    feat: configure crud operations in TaskController class

--- backend/src/main/java/org/amida/backend/controller/TaskController.java
+++ backend/src/main/java/org/amida/backend/controller/TaskController.java
@@ -1,4 +1,78 @@
 package org.amida.backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.amida.backend.model.Task;
+import org.amida.backend.model.User;
+import org.amida.backend.request.TaskRequest;
+import org.amida.backend.response.ApiResponse;
+import org.amida.backend.response.TaskResponse;
+import org.amida.backend.service.TaskService;
+import org.amida.backend.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/task")
+@Slf4j
 public class TaskController {
+
+    private final TaskService taskService;
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(
+            @RequestBody TaskRequest request,
+            @AuthenticationPrincipal UserDetails userDetails) { // fix: pass the UserDetails parameter
+        // along with the UserDetailsImpl
+
+        String username = userDetails.getUsername();
+        User user = userService.findUserByUsername(username);
+        log.info("Finding user ");
+        TaskResponse response = taskService.createTask(user, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponse> updateTask(@AuthenticationPrincipal UserDetails userDetails, Long taskId, TaskRequest request) {
+        String username = userDetails.getUsername();
+        User user = userService.findUserByUsername(username);
+        TaskResponse response = taskService.updateTask(user, taskId, request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TaskResponse> deleteTask(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails){
+        String username = userDetails.getUsername();
+        User user = userService.findUserByUsername(username); // можно вынести логику экстракта в отдельный прайват метод
+        TaskResponse response = taskService.deleteTask(user, id);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(@AuthenticationPrincipal UserDetails userDetails){
+        String username = userDetails.getUsername();
+        User user = userService.findUserByUsername(username);
+
+        return ResponseEntity.ok(taskService.getAllTasksByUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails){
+        String username = userDetails.getUsername();
+        User user = userService.findUserByUsername(username);
+
+        return ResponseEntity.ok(taskService.getTaskByIdAndUser(user, id));
+    }
+
+
 }
